60dceebc925670d1abca6f918e9fdf7fc63793aa
wkm23@126.com <wkm23@126.com>
2017-07-07 00:54:25 +1200

    add test

--- app.js
+++ app.js
@@ -1,88 +1,11 @@
-
-const curLocation = {
-  x: 0,
-  y: 0,
-  f: 0
-};
-
-const DIRECTION = {
-  NORTH: 0,
-  EAST: 1,
-  SOUTH: 2,
-  WEST: 3
-};
-
-const DIR_ARR = ['NORTH', 'EAST', 'SOUTH', 'WEST'];
-
-const SQUARE = {
-  x: 4,
-  y: 4
-};
-
-module.exports.game = function game(command) {
-  switch (true) {
-    case command.includes('PLACE'):
-      const pos = command.replace(/PLACE| /g, '').split(',');
-      const x = parseInt(pos[0]), y = parseInt(pos[1]);
-      if (x <= SQUARE.x && y <= SQUARE.y && x >= 0 && y >= 0) {
-        curLocation.f = DIRECTION[pos[2]];
-        curLocation.x = x;
-        curLocation.y = y;
-        move(pos[2]);
-      }
-      break;
-    case command === 'MOVE':
-      move(curLocation.f, 1);
-      break;
-    case command === 'LEFT':
-      curLocation.f = (curLocation.f + 4 - 1) % 4;
-      break;
-    case command === 'RIGHT':
-      curLocation.f = (curLocation.f + 4 + 1) % 4;
-      break;
-    case command === 'REPORT':
-      console.log('CURRENT LOCATION:', curLocation.x, curLocation.y, DIR_ARR[curLocation.f]);
-      break;
-  }
-
-}
-
-
-function move(face = throwError(), step = 0) {
-  switch (face) {
-    case 0:
-      if (curLocation.y + step <= SQUARE.y) {
-        curLocation.y += step;
-      }
-      break;
-    case 1:
-      if (curLocation.x + step <= SQUARE.x) {
-        curLocation.x += step;
-      }
-      break;
-    case 2:
-      if (curLocation.y - step >= 0) {
-        curLocation.y -= step;
-      }
-      break;
-    case 3:
-      if (curLocation.x - step >= 0) {
-        curLocation.x -= step;
-      }
-      break;
-  }
-}
-
-function throwError() {
-  throw new Error('Missing parameter');
-}
+const game = require('./game');
 
 process.stdin.resume();
 process.stdin.setEncoding('utf8');
 
 let step = 1;
 const commands = [];
-function ask() {
+module.exports.ask = function ask() {
   var stdin = process.stdin, stdout = process.stdout;
 
   stdin.resume();
@@ -99,13 +22,13 @@ function ask() {
         console.log('Start your game by placing your robot, please.');
       } else {
         commands.push(data);
-        game(data);
+        game.play(data);
       }
       ask();
     }
   });
-}
+};
 
 
 console.log('Input steps one by one in command line as prompt.\nWriting END to finish the game\nInitial default location is 0, 0, NORTH for only PLACE COMMAND');
-ask();
+this.ask();
--- test.js
+++ test.js
@@ -1,3 +1,160 @@
 const should = require('should');
+const sinon = require('sinon');
 
-const game = require('app');
+const game = require('./game');
+
+describe('Game tests ', () => {
+  describe('curLocation', () => {
+    it('should have init value', () => {
+      const target = {
+        x: 0,
+        y: 0,
+        f: 0
+      };
+      game.curLocation.should.be.eql(target);
+    })
+  });
+
+  describe('DIRECTION', () => {
+    it('should have init value', () => {
+      const target = {
+        NORTH: 0,
+        EAST: 1,
+        SOUTH: 2,
+        WEST: 3
+      };
+      game.DIRECTION.should.be.eql(target);
+    })
+  });
+
+  describe('SQUARE', () => {
+    it('should have init value', () => {
+      const target = {
+        x: 4,
+        y: 4
+      };
+      game.SQUARE.should.be.eql(target);
+    })
+  });
+
+  describe('Place function', () => {
+    it('should not change position with invalid place', () => {
+      const command = 'PLACE 6, 7, NORTH';
+      const target = {
+        x: 0,
+        y: 0,
+        f: 0
+      };
+      game.play(command);
+      game.curLocation.should.be.eql(target);
+    });
+
+    it('should have a postion with valid place', () => {
+      const command = ['PLACE 4, 4, NORTH'];
+      const target = {
+        x: 4,
+        y: 4,
+        f: 0
+      };
+      game.play(...command);
+      game.curLocation.should.be.eql(target);
+    });
+
+    it('should MOVE one step to NORTH', () => {
+      const command1 = 'PLACE 0,0,NORTH';
+      const command2 = 'MOVE';
+      const target = {
+        x: 0,
+        y: 1,
+        f: 0
+      };
+      game.play(command1);
+      game.play(command2);
+      game.curLocation.should.be.eql(target);
+    });
+
+    it('should turn LEFT to WEST when face to NORTH', () => {
+      const command1 = 'PLACE 0,0,NORTH';
+      const command2 = 'LEFT';
+      const target = {
+        x: 0,
+        y: 0,
+        f: 3
+      };
+      game.play(command1);
+      game.play(command2);
+      game.curLocation.should.be.eql(target);
+    });
+
+    it('should turn RIGHT to EAST when face to NORTH', () => {
+      const command1 = 'PLACE 0,0,NORTH';
+      const command2 = 'RIGHT';
+      const target = {
+        x: 0,
+        y: 0,
+        f: 1
+      };
+      game.play(command1);
+      game.play(command2);
+      game.curLocation.should.be.eql(target);
+    });
+
+    it('should REPORT current location', () => {
+      const command1 = 'PLACE 0,0,NORTH';
+      const command2 = 'REPORT';
+      const spy = sinon.spy(console, 'log');
+      game.play(command1);
+      game.play(command2);
+      spy.calledOnce.should.be.true();
+      spy.restore();
+    });
+  });
+
+  describe('Move function', () => {
+    beforeEach(() => {
+      const command = 'PLACE 0, 0, NORTH';
+      game.play(command);
+    });
+    it('should have one step to EAST', () => {
+      game.move(1, 1);
+      game.curLocation.x.should.be.eql(1);
+    });
+    it('should have one step to NORTH', () => {
+      game.move(0, 1);
+      game.curLocation.y.should.be.eql(1);
+    });
+    it('should have one step to SOUTH', () => {
+      const command = 'PLACE 2, 2, NORTH';
+      game.play(command);
+      game.move(2, 1);
+      game.curLocation.y.should.be.eql(1);
+    });
+    it('should have one step to WEST', () => {
+      const command = 'PLACE 2, 2, NORTH';
+      game.play(command);
+      game.move(3, 1);
+      game.curLocation.x.should.be.eql(1);
+    });
+    it('should not have one step to WEST', () => {
+      game.move(3, 1);
+      game.curLocation.x.should.be.eql(0);
+    });
+    it('should not have one step to SOUTH', () => {
+      game.move(2, 1);
+      game.curLocation.y.should.be.eql(0);
+    });
+    it('should not have one step to NORTH', () => {
+      const command = 'PLACE 2, 4, NORTH';
+      game.play(command);
+      game.move(0, 1);
+      game.curLocation.y.should.be.eql(4);
+    });
+    it('should not have one step to EAST', () => {
+      const command = 'PLACE 4, 4, NORTH';
+      game.play(command);
+      game.move(1, 1);
+      game.curLocation.x.should.be.eql(4);
+    });
+  });
+
+});
